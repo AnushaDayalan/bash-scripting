b1b508e20d7d8598c64aca2e109127e20096c372
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-10-30 13:13:52 -0400

    new

--- sample.sh
+++ sample.sh
@@ -1,13 +1,1 @@
-echo they there welcome to cloud
-echo -e "linus is very important  
-echo -e "Linus1 \n Linus 2 linus 3"
-
-echo -e " 10 \n 12 13 15 16"
-
-echo hi there
-echo -e "line 1 \n line 2 \n line 3"
-echo "Anusha is the best"
-echo "run new file \n new file , page line "
-
-
-git add
+echo -e "Line5\nLine6"
