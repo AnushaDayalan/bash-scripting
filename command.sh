4254ba6528bc1129f62f9d38cb427c62bf72f00c
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-01 15:59:09 -0400

    code

--- command.sh
+++ command.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-Date="$(date +%C)"
-echo -e "\e [34m Today date is $Date \e[0m"
+Date="$(date +%F)"
+
+echo -e "\e[34;44m Today date is $Date \e[0m"
+
+#echo -e "\e[33:43m Print these two colours \e[0m"
 
