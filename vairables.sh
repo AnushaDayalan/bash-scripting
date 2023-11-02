39a435b1edb17039e3b977c26bfe4ebd0c76de3f
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 10:32:49 -0400

    f

--- vairables.sh
+++ vairables.sh
@@ -3,4 +3,8 @@ Session_Count=$(who | wc -l)
 echo " Type current is = $Session_Count"
 
 echo -e "\e[33m Type current count  is $Session_Count \e[0m"
-                
+
+Date=$(date +%F)
+
+echo "Print date = $Date"
+
