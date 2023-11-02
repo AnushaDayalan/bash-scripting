60aa593ccc3a0c351df8b62845783ab4913ebabc
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 11:21:57 -0400

    f

--- sample.sh
+++ sample.sh
@@ -1,8 +1,8 @@
 Session_Count=$(who | wc -l)
 
-echo -e "\e Print the sessioncounts as = $Session_Count"
+echo -e "\e PPPrint the sessioncounts as = $Session_Count"
 
 Date=$(date +%F)
 
-echo -e "\e Print todays date =$1"
+echo -e "\e PPPrint todays date =$1"
 
