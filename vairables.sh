c46e36841245dfa3897b70c4966c875d04d82c28
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 13:19:45 -0400

    f

--- vairables.sh
+++ vairables.sh
@@ -2,9 +2,10 @@ Session_Count=$(who | wc -l)
 
 echo -e "\e Print the sessioncounts as =$Session_Count"
 
-echo "\e Print collelge name =$1"
-echo "\e Print logo name =$2"
+echo -e "\e Print collelge name =$1"
+echo -e "\e Print logo name =$2"
 echo $#
 echo $$
 echo $?
 echo $@
+echo $$
