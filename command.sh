2ae650053ced33b8e71ab749f7094b71ecc5fdf9
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 10:55:51 -0400

    f

--- command.sh
+++ command.sh
@@ -6,3 +6,10 @@ echo -e "\e[33;44m Today date is $Date \e[0m"
 
 #echo -e "\e[33:43m Print these two colours \e[0m"
 
+
+Date="$(date +%F)"
+
+echo "Print  date is = $Date"
+
+Talented_Guest=\e$(Anusha)
+echo "\ePrint talented guest =$1"
--- vairables.sh
+++ vairables.sh
@@ -4,10 +4,3 @@ echo " Type current is = $Session_Count"
 
 echo -e "\e[33m Type current count  is $Session_Count \e[0m"
 
-Date="$(date +%F)"
-
-echo "Print  date is = $Date"
-
-Talented_Guest= -e "\e$(Anusha)"
-echo "\ePrint talented guest =$1"
-
