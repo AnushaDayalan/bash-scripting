acb1e397fab381df1eedccba125fe1144a5752dd
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 11:46:56 -0400

    f

--- sample.sh
+++ sample.sh
@@ -4,8 +4,9 @@ echo -e "\e PPrint the sessioncounts as = $Session_Count"
 
 Date=$(date +%F)
 
-echo -e "\e PPrint todays date =$Date"
 
+echo -e "\e PPrint todays date =$Date"
+echo $0
 #Talented_Person=$Anusha
 
 echo -e "\e PPrint talented person =$0"
