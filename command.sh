15287a44c77df2c7728020fd45fc336a3042af1c
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 11:10:43 -0400

    f

--- command.sh
+++ command.sh
@@ -7,9 +7,9 @@ echo -e "\e[33;44m Today date is $Date \e[0m"
 #echo -e "\e[33:43m Print these two colours \e[0m"
 
 
-Date="$(date +%F)"
+Date="\e$(date +%F)"
 
-echo "Print  date is = $Date"
+echo "\ePrint  date is = $Date"
 
-Talented_Guest=\e$(Anusha)
-echo "\ePrint talented guest =$1"
+Talented_Guest="\e$(Anusha)"
+echo -e "\e Print talented guest = $1\e"
