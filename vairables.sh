fe50be374ad302af961ed720c9dfcf8ed10dce23
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 15:02:05 -0400

    f

--- vairables.sh
+++ vairables.sh
@@ -5,19 +5,15 @@ stat () {
 echo -e "\e Print Session_Count=$(who | wc -l)"
 
 echo -e "\e Print date is $(date +%F)"
+}
 
 sleep 10
 
 stat
 
 sleep 13
-
+ 
 stat 
-
-}
-
-
-
 # echo -e "\e Print collelge name =$1"
 # echo -e "\e Print logo name =$2"
 # echo $#
