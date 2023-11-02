4244a0ed346845737a5a37ff755488563e5e697c
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-02 11:14:54 -0400

    f

--- vairables.sh
+++ vairables.sh
@@ -4,9 +4,10 @@ echo " Type current is = $Session_Count"
 
 echo -e "\e[33m Type current count  is $Session_Count \e[0m"
 
-Date="\e$(date +%F)"
+Date= "\e$(date +%F)"
 
-echo "\ePrint  date is = $Date"
+echo -e "\e Print  date is = $Date \e"
 
 Talented_Guest= "\e$(Anusha)"
+
 echo -e "\e Print talented guest = $1\e"
