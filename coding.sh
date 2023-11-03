4079e90fd2095e7abb55516c16f7cdc1b6413934
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-03 12:35:27 -0400

    f

--- coding.sh
+++ coding.sh
@@ -12,6 +12,8 @@ hello() {
     echo "learning daily linux is useful for $2"
 
 }
- echo "\e show uptime $(uptime | awk  -F : '{print $1}'
 
- 
+hello
+stat
+echo "\e show uptime $(uptime | awk  -F : '{print $1}')"
+
