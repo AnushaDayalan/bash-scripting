5ab0fc46c5cbda8670cfb518d4ddf759d1889c99
Bala Kumar Jayaraman <anushadayalan90@gmail.com>
2023-11-06 13:41:06 -0500

    gt

--- studentapp/frontendhttp.sh
+++ studentapp/frontendhttp.sh
@@ -1,12 +1,13 @@
-ACTION=$1
+start() 
 
-start() {
+{
 
-echo -e "\e Print this =$2"
+echo -e "\e Prigitnt this =$2"
 
 echo "\e[31m Git it correct\e[0m"
 
 echo -e "\e[34m Show todays date =$(date +%F)\e[0m"
 
 }
+
 stop()
