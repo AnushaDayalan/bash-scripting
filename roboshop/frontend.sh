05fde760e3ded551c7c5d145dacdee7c39a1ff4d
anusha <balak@JBK.com>
2023-11-29 18:46:06 -0500

    f

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,15 +1,4 @@
 
-
-mv static/* .
-rm -rf frontend-master README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>> /tmp/frontend .log
-
-if [ $? -eq 0 ] ; then
- echo -e "\e[36m SUCCESS \e[0m"
-    else
- echo -e "\e[33m  FAILURE \e[0m"
-fi
-
 echo -e -n"\e[33m Restarting the nginx $1 \e[0m"
 
 systemctl enable nginx   &>> /tmp/frontend .log
