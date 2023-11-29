a2ba32c6b1eceed6b56f3ec4a64b9238e6c3e78b
anusha <balak@JBK.com>
2023-11-29 14:40:37 -0500

    in

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo -e "******** \e[35m Configuration FRONTEND \e[0m******"
+
+echo -e "Installing nginx :"
+
+# yum install nginx -y
+# systemctl enable nginx 
+# systemctl start nginx 
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# cd /usr/share/nginx/html
+# rm -rf * 
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-master README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
