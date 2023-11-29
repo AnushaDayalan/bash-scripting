5fe5b913319c0f3adceecc9e179256b626c6336b
anusha <balak@JBK.com>
2023-11-29 17:04:38 -0500

    g

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-
+Component = $1
 if [ $USER_ID -ne 0 ] ; then
 echo -e "\e[33m This script is pritned as non sudo user\e[0m"
 echo -e "\e example : \n \t\t sudo bash comp"
@@ -18,3 +18,13 @@ if [ $? -eq 0 ] ; then
     else
  echo -e "\e[33m Nginx FAILURE \e[0m"
 fi
+
+echo  -n "Downloading the Component = $1:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+if [ $? -eq 0 ] ; then
+ echo -e "\e[36m Nginx installing successfully \e[0m"
+    else
+ echo -e "\e[33m Nginx FAILURE \e[0m"
+fi
+exit 2
