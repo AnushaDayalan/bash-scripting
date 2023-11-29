ab27fc8768d755b4288a26e02aab1eb30c70322e
anusha <balak@JBK.com>
2023-11-29 17:33:10 -0500

    g

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -32,3 +32,37 @@ fi
 echo "\n Cleaning up the component $1"
 cd /usr/share/nginx/html
 rm -rf * 
+
+if [ $? -eq 0 ] ; then
+ echo -e "\e[36m SUCCESS \e[0m"
+    else
+ echo -e "\e[33m  FAILURE \e[0m"
+fi
+
+echo -e "\n[31m UNZIP the frontend $1\e[0m"
+unzip /tmp/frontend.zip
+
+echo -n "\e Configuring $1"
+
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+if [ $? -eq 0 ] ; then
+ echo -e "\e[36m SUCCESS \e[0m"
+    else
+ echo -e "\e[33m  FAILURE \e[0m"
+fi
+
+echo -e "\n[13m Restarting the nginx[0m"
+
+systemctl enable nginx 
+systemctl daemon-reload nginx
+systemctl re-start nginx 
+
+if [ $? -eq 0 ] ; then
+ echo -e "\e[36m SUCCESS \e[0m"
+    else
+ echo -e "\e[33m  FAILURE \e[0m"
+fi
