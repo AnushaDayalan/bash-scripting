6b94f37e1aebe6e506a6826813dc4bd8e244dfec
anusha <balak@JBK.com>
2023-11-29 16:11:52 -0500

    d

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+UID=$(id -u)
+if
+[ $UID -ne 0] ; then
+
+echo -e "\e [33m This script is pritned as non sudo user\e[0m"
+ exit 1
+
+fi
 
 echo -e "******** \e[35m Configuration FRONTEND \e[0m******"
 
