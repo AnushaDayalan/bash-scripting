e6551ffbec1f8279c08e31111929f90eceaca35b
anusha <balak@JBK.com>
2023-11-29 16:48:52 -0500

    f

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -11,4 +11,11 @@ fi
 
 echo -e "******** \e[35m Configuration frontend \e[0m******"
 echo  "Installing nginx"
-yum install nginx -y  > /tmp/frontend .log
+yum install nginx -y  &>> /tmp/frontend .log
+
+if
+ [$? -eq 0] ; then
+ echo -e "\e[36m Nginx installing successfully \e[0m"
+    else
+ echo -e "\e[33m Nginx FAILURE \e[0m"
+fi
