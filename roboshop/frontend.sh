6c5a2d8ecc6b95269d0045590a63578bc5e100f3
anusha <balak@JBK.com>
2023-11-29 18:47:17 -0500

    g

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,7 +3,7 @@ echo -e -n"\e[33m Restarting the nginx $1 \e[0m"
 
 systemctl enable nginx   &>> /tmp/frontend .log
 systemctl daemon-reload   &>> /tmp/frontend .log
-systemctl restart nginx      &>> /tmp/frontend .log
+systemctl start nginx      &>> /tmp/frontend .log
 ##systemctl restart nginx    &>> /tmp/frontend .log
 
 if [ $? -eq 0 ] ; then
