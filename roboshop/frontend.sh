3bb8f30fdfad354169b745f5df1ef183dd982db7
anusha <balak@JBK.com>
2023-11-29 17:05:34 -0500

    g

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -23,7 +23,7 @@ echo  -n "Downloading the Component = $1:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 if [ $? -eq 0 ] ; then
- echo -e "\e[36m Nginx installing successfully \e[0m"
+ echo -e "\e[36m COMPONENT SUCCESSFULLY DOWNLOADED \e[0m"
     else
  echo -e "\e[33m Nginx FAILURE \e[0m"
 fi
