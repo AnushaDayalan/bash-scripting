be9d24bed84463a4e16535eb79be62348067e815
anusha <balak@JBK.com>
2023-11-29 16:29:00 -0500

    f

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,13 +3,8 @@
 UID=$(id -u)
 
 if [ $UID -ne 0 ] ; then
-
 echo -e "\e[33m This script is pritned as non sudo user\e[0m"
 echo -e "\e[31m example : \n \t\t \e[0m" sudo bash comp"
 exit 1
 
 fi
-
-echo -e "******** \e[35m Configuration FRONTEND \e[0m******"
-echo  "Installing nginx"
-yum install nginx -y
