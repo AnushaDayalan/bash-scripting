dfa62d1a4b27c5d229a5123de1b5f4b52612abba
anusha <balak@JBK.com>
2023-11-29 22:30:23 -0500

    g

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -1,5 +1,5 @@
 
-systemctl enable mongod  $(LOGFILE)
-systemctl daemon-reload mongod $(LOGFILE)
-systemctl start mongod   $(LOGFILE)
-stat $?
+systemctl enable mongod  ##$(LOGFILE)
+systemctl daemon-reload mongod ##$(LOGFILE)
+systemctl start mongod   ##$(LOGFILE)
+##stat $?
