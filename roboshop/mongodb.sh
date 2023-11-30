b955410dc2d4675bb9c2f4e816bff2dc7475445b
anusha <balak@JBK.com>
2023-11-29 20:47:34 -0500

    Gg

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -1,14 +1,4 @@
+#!/bin/bash
 
-USER_ID=$(id -u)
-
-if [ $USER_ID -ne 0 ] ; then
-
-echo -e -n "\e[31m DO not install MONGODB \e[0m"
-
-else
-echo -e -n "\e[33m MONGODB INSTALLED SUCCESSFULLY \e[0m"
-
-fi
-exit 1
 
 yum install -y mongodb-org  
