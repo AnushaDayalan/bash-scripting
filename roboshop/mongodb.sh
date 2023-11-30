be72404b31610e193594ebcd46e4b93f8589baf3
anusha <balak@JBK.com>
2023-11-29 21:28:16 -0500

    g

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-yum install -y mongodb-org  
+USER_ID=$(id -u)
+COMPONENT=mongodb
+LOGFILE= "/tmp/$(COMPONENT).log"
+REPO = "https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+stat() { 
 
-if [ $? -eq 1 ] ; then
-echo -e "\e Success"
+    if [$1 -eq 0 ] ; then
+        echo -e "\e success"
 
-else
+    else
+        echo -e "\e fail"
+}
 
-echo "\e Fail"
 
+if  [ $USER_ID -ne 0 ]; then
+
+    echo -n "\e need to run as sudo"
 fi
-
 exit 1
+
+echo "****** \e confguring $(COMPONENT) repo ******"
+curl -s -o /etc/yum.repos.d/mongodb.repo $REPO
+stat $?
+
+echo -e "\e Installing $(COMPONENT)"
+yum install -y mongodb-org 
+
