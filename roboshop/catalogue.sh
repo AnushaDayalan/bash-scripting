cb2589b23d93702de2e127d65bc6e160063d3a19
anusha <balak@JBK.com>
2023-12-02 23:30:42 -0500

    g

---
+++ roboshop/catalogue.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+##USER_ID=
+COMPONENT=catalogue
+URL="https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm"
+LOGFILE="/tmp/$(COMPONENT).log"
+
+stat()
+{ 
+    if [$# -eq 0] then
+
+echo -e -n "\e [33m Success\e[0m"
+
+else
+
+echo -e -n "\e [34m Fail\e[0m"
+fi
+} 
+
+echo -n "\e[31m Installation of NODEJS for $COMPONENT \e[0m"
+yum install nodejs -y &>> $LOGFILE
