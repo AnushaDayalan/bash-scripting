5179285700b9e05f25056c44dcbbff920f066e3a
anusha <balak@JBK.com>
2023-12-02 23:42:49 -0500

    h

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-##USER_ID=
+USER_ID=$(-id -u)
 COMPONENT=catalogue
 URL="https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm"
 LOGFILE="/tmp/$COMPONENT.log"
+
+if 
+[ USER_ID -ne 0 ]; then
+echo -n "\e[35m Print as sudo user\e[0m"
+exit 1
+fi
 
 stat()
 { 
@@ -17,4 +23,4 @@ fi
 } 
 
 echo -n "\e[31m Installation of NODEJS for $COMPONENT \e[0m"
-yum install nodejs -y &>> $LOGFILE
+yum install nodejs -y $URL &>> $LOGFILE
